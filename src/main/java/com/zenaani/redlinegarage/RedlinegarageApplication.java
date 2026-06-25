48236eebcf798d167801fe118e74ca2c79c22863
zenaani <zenaani.s@applab.qa>
2026-06-25 14:49:28 +0530

    moved cred to .env

--- src/main/java/com/zenaani/redlinegarage/RedlinegarageApplication.java
+++ src/main/java/com/zenaani/redlinegarage/RedlinegarageApplication.java
@@ -2,8 +2,10 @@ package com.zenaani.redlinegarage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
 public class RedlinegarageApplication {
 
 	public static void main(String[] args) {
