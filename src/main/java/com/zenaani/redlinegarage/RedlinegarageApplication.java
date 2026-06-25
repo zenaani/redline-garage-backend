e05ad3716caea4dd619b1b96088acf555daa585d
zenaani <zenaani.s@applab.qa>
2026-06-25 15:27:54 +0530

    remove properysource

--- src/main/java/com/zenaani/redlinegarage/RedlinegarageApplication.java
+++ src/main/java/com/zenaani/redlinegarage/RedlinegarageApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
+//@PropertySource(value = "file:.env", ignoreResourceNotFound = true)
 public class RedlinegarageApplication {
 
 	public static void main(String[] args) {
